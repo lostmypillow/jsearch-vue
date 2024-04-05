544d4c4f905984b7f9103cf9081dd5530b16c0d8
lostmypillow <47119111+lostmypillow@users.noreply.github.com>
2024-04-05 22:16:44 +0800

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,37 +1,18 @@
 <script setup>
-import { ref } from 'vue'
-import MainPage from './components/MainPage.vue'
-import TopSection from './components/TopSection.vue'
-import InputWrapper from './components/InputWrapper.vue'
-import APIInput from './components/APIInput.vue'
-import SearchInput from './components/SearchInput.vue'
-import MovieView from './components/MovieView.vue'
-import AppTitle from './components/AppTitle.vue'
-
-
-
-// const movies = ref([]);
 const title = ref("");
-const apikey = ref("");
+// const apiKey = ref("");
 const searchTerm = ref("");
-const API_URL = ref("")
-const showSearch = ref(false);
-function hide(n) {
-   showSearch.value = !showSearch.value;
-// console.log(n.value)
-//  apikey.value = n
-// API_URL.value += apikey.value
-// console.log(API_URL.value)
-API_URL.value = "https://www.omdbapi.com/?apikey=" + apikey.value
- };
+const keyExists= ref(false);
+
 async function searchMovies() {
-
-  const response = await fetch(`${API_URL.value}&s=${title.value}`)
-  const json = await response.json();
-  console.log(json.Search)
-
+  try {
+    const response = await axios.get('https://www.omdbapi.com/?apikey=&s=batman');
+    console.log(response.toJSON());
+  } catch (error) {
+    console.error(error);
+  }
 }
-
+ 
 </script>
 
 <template>
@@ -41,8 +22,10 @@ async function searchMovies() {
 
       <InputWrapper>
 
-        <APIInput v-if="showSearch == false" @show-search="hide" v-model="apikey"  />
-        <SearchInput v-else @search-movies="searchMovies(title)" v-model="title" />
+        <APIInput v-if="keyExists == false" @show-search="hide" v-model="apikey"  />
+    <APIButton @click="keyExists == !keyExists" />
+        <SearchInput v-else  v-model="title" />
+    <SearchButton />
 
       </InputWrapper>
     </TopSection>
