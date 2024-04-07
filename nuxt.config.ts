055ed72c6cf7653358a4ca84259ff36fa991b3e9
LostMyPillow <47119111+lostmypillow@users.noreply.github.com>
2024-04-08 01:58:21 +0800

    update base URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {baseURL: "/JSearch"},
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   postcss: {
