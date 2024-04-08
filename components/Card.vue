9e06e539689ae6168e0f32a9e30a4a046ddd4f77
LostMyPillow <47119111+lostmypillow@users.noreply.github.com>
2024-04-08 14:53:41 +0800

    04081453

---
+++ components/Card.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps(['smalltext', 'title', 'subtitle', 'image'])
+
+
+</script>
+
+<template>
+    <div
+        class="flex flex-row items-center justify-start gap-4 border-2 border-rose-400 mx-2 my-4 p-2 rounded-2xl backdrop-blur-3xl">
+        <div class="shrink-0">
+            <img class="h-11" :src="image" alt="">
+        </div>
+
+        <div>
+            <h3 class="text-sm font-light">
+                {{ smalltext }}
+            </h3>
+            <div class="text-lg">
+                {{ title }} <br> ({{ subtitle }})
+            </div>
+        </div>
+
+    </div>
+</template>
