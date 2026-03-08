0754a400350d1dfbad5e6ef7537db8c04270b5c4
Johnny <jmlin0101@gmail.com>
2026-03-08 23:39:51 +0800

    Added health check to website

--- app.vue
+++ app.vue
@@ -39,6 +39,29 @@ function onClick() {
 
 
 }
+const isReady = ref(false);
+
+const waitForHealthCheck = async (url) => {
+  while (!isReady.value) {
+    try {
+      const response = await fetch(url);
+
+      if (response.ok) {
+        isReady.value = true;
+        console.log('Endpoint is healthy!');
+        break;
+      }
+    } catch (error) {
+      console.warn('Endpoint not ready, retrying in 2 seconds...');
+    }
+
+    await new Promise(resolve => setTimeout(resolve, 2000));
+  }
+};
+
+onMounted(() => {
+  waitForHealthCheck('https://jsearch-api.onrender.com/health');
+})
 
 </script>
 
@@ -49,7 +72,7 @@ function onClick() {
       <v-app-bar scroll-behavior="elevate" rounded>
 
         <v-app-bar-title><v-btn variant="text" to="/">
-            <label class="text-2xl">JSEARCH</label>
+            <label class="text-2xl">JSEARCH (API is {{isReady? 'online' : 'offline, please wait...'}})</label>
           </v-btn></v-app-bar-title>
 
         <template v-slot:append>
@@ -64,15 +87,17 @@ function onClick() {
 
       <div class="flex flex-col w-svw items-center justify-center pt-16 px-6 pb-6">
         <v-text-field v-model="searchTerm" class="flex w-full md:w-1/4 h-11 pt-2" :label=searchLabel type="input"
-          append-icon="mdi-send" @click:append="onClick" @keyup.enter="onClick" :loading="loading" />
+          append-icon="mdi-send" @click:append="onClick" @keyup.enter="onClick" :loading="loading" :readonly="isReady" />
 
         <v-select v-model="searchType" class="flex w-full pt-6 md:w-1/4" label="Change Search Type"
-          :items="['movies', 'google']" variant="underlined" />
+          :items="['movies', 'google']" variant="underlined"
+        :readonly="isReady"
+        />
 
 
         <div class="flex flex-col pt-4 w-full items-center">
 
-        
+
 
           <NuxtPage  />
 
